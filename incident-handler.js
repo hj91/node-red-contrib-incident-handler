548fb48a017e5593d02adb1be4fe331a4cdaf877
harshad joshi <firewalrus@gmail.com>
2023-04-23 18:58:09 +0530

    License Notice update

--- incident-handler.js
+++ incident-handler.js
@@ -1,8 +1,26 @@
+/**
+
+ Copyright 2023 Bufferstack.IO Analytics Technology LLP, Pune
+
+ Licensed under the GNU General Public License, Version 3.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ https://www.gnu.org/licenses/gpl-3.0.html
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ **/
+
+
+
 // incident-handler.js
 // Harshad Joshi, Bufferstack.IO Analytics Technology LLP
 // Release date - 23 April 2023
-// License - https://github.com/hj91/node-red-contrib-incident-handler/LICENSE 
-// Released under GPL 3.0
 
 
 module.exports = function (RED) {
